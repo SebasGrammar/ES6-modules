9046b321c05c7d3be1012de960520af5b8918427
Sebastian <sebasgrammar@hotmail.com>
2020-01-31 22:00:01 -0500

    Elaborated on an important point

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-// https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
-
 import iCanHaveAnyName from './module.js'; // As opposed to named imports, default imports can have any name you want.
 // Another key difference between these two is that named imports have to be enclosed in brackets.
 import {myNameCanChange as myNameChanged} from './module.js'
-import {myNameMustMatch} from './module.js' // named imports have to actually match exports in the module.
-// import myNameMustMatch from "./module.js" X won't work.
+import {myNameMustMatch} from './module.js' // A named import has to actually match an export in the module.
+// import myNameMustMatch from "./module.js" X won't work. Don't forget to put in the brackets.
 import * as everySingleExport from "./module.js" // This is an object containing every single export from the specified module.
-
 import {logOne, logTwo, addOne} from "./module.js" // It is possible to import multiple exports by separating
 // them with commas.
 import {one, two} from "./module.js"
-
-console.log(everySingleExport.three) // These values are read only. They can't be modified from outside
+/*****************************************************************************************************/
+console.log(everySingleExport.three) // These values are read only. They can't be modified from outside the module
+// if not through a function defined in the same module.
 console.log(nice) // Didn't even have to import this one because it's global.
 
 myNameMustMatch(myNameMustMatch)
@@ -52,4 +49,6 @@ let test = {
 
 ({b: a} = test) // This is how you reassign a value to a previously defined variable
 // by using destructuring.
+
+console.log(a)
 
